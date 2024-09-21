a0f4691c28d8be9d4e5e99552368c2aa6cab67bd
nisanurgunes <77933400+nisanurgunes@users.noreply.github.com>
2024-09-21 20:32:07 +0300

    Add files via upload

---
+++ jenkins/src/test/java/com/jnkns/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnkns.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
